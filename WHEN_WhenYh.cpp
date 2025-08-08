02d45e17ee5a47062622ab901f0dbb80cd2f5cee
ZetterZeck <shadowinthedarkness1771@gmail.com>
2025-08-08 17:40:13 +0700

    Add files via upload

---
+++ WHEN_WhenYh.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    
+    cout << "WHEN YH" << endl;
+    
+    cout << endl;
+    for (int i = 1; i > 0; "yh")
+    {
+        cout << "W             W      H         H      EEEEEEEEEEEE      N N       N\n";
+        cout << "W             W      H         H      E                 N  N      N\n";
+        cout << "W             W      H         H      E                 N   N     N\n";
+        cout << "W      W      W      HHHHHHHHHHH      E                 N    N    N\n";
+        cout << "W    W  W     W      H         H      EEEEEEEEEEEE      N     N   N\n";
+        cout << "W   W     W   W      H         H      E                 N      N  N\n";
+        cout << "W W         W W      H         H      E                 N       N N\n";
+        cout << "W            WW      H         H      EEEEEEEEEEEE      N        N\n";
+        cout << "-------------------------------------------------------------------\n";
+        cout << "W             W      H         H      EEEEEEEEEEEE      N N       N\n";
+        cout << "W             W      H         H      E                 N  N      N\n";
+        cout << "W             W      H         H      E                 N   N     N\n";
+        cout << "W      W      W      HHHHHHHHHHH      E                 N    N    N\n";
+        cout << "W    W  W     W      H         H      EEEEEEEEEEEE      N     N   N\n";
+        cout << "W   W     W   W      H         H      E                 N      N  N\n";
+        cout << "W W         W W      H         H      E                 N       N N\n";
+        cout << "W            WW      H         H      EEEEEEEEEEEE      N        N\n";
+        cout << "\n\n";
+    }
+}
